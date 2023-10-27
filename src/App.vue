3e5d8b32b9d3298b89c9f3126f5c70a79bb97fb7
soareslss <soareslssa@fab.mil.br>
2023-10-27 11:50:58 -0300

    feat: implement menu and logo

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,13 @@
 
 <script>
 import { defineComponent } from 'vue'
+import { useQuasar } from 'quasar';
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup () {
+    const $q = useQuasar()
+    $q.dark.set(true)
+  }
 })
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,8 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+      alt="Encontro UJ24"
+      src="~assets/unico-logo.png"
     >
   </q-page>
 </template>

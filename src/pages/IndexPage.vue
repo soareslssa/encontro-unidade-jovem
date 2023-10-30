fba0c03e4e9f63d3daf45f0c9be15728ae3c1ce6
soareslss <soareslssa@fab.mil.br>
2023-10-30 12:06:50 -0300

    feat: add awaken component

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,27 @@
 <template>
   <q-parallax src="~assets/encontro.jpg" :height="1000">
-    <img alt="Encontro UJ24" src="~assets/unico-logo.png" />
+    <div class="row text-center items-center">
+      <div class="col-12 col-md-2"></div>
+      <div class="col-12 col-md-6">
+        <img alt="Encontro UJ24" src="~assets/unico-logo.png" />
+        <q-btn
+          label="INSCREVA-SE AGORA"
+          style="opacity: 75%"
+          size="xl"
+          rounded
+          color="primary"
+        />
+      </div>
+      <div class="col-12 col-md-2 text-h2 text-center">
+        <strong>19, 20 e 21</strong>
+        <section class="text-h4">
+          <strong>DE OUTUBRO</strong>
+        </section>
+      </div>
+    </div>
   </q-parallax>
   <countdown />
+  <q-img src="~/assets/awaken-desktop.gif"/>
 </template>
 
 <script>
